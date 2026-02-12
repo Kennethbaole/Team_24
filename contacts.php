8397169e7795307c280d1ad30456849138ad6768
Jase Thomas <98228547+JaseThomas04@users.noreply.github.com>
2026-02-12 16:27:41 -0500

    Update contacts.php
    Added semicolons at ends of headers, fixed parameter on line 8

--- contacts.php
+++ contacts.php
@@ -1,11 +1,11 @@
 <?php
 
-header('Access-Control-Allow-Origin: *')
-header('Content-Type: application/json')
-header('Access-Control-Allow-Method: GET')
-header('Access-Control-Allow-Headers: ')
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Method: GET');
+header('Access-Control-Allow-Headers: ');
 
-$requestMethod = $_SERVER["REQUEST METHOD"];
+$requestMethod = $_SERVER["REQUEST_METHOD"];
 
 if ($requestMethod == "GET") {
 

2222032bf47774bdb4838de48ce0918360613c4e
KennethBaoLe <kennethble@gmail.com>
2026-02-13 14:17:24 -0500

    Adjusted mysqli() param order, syntax issue, and added 5 params for bind_param()

--- searchNames.php
+++ searchNames.php
@@ -1,86 +1,88 @@
 <?php
 
 $host = "localhost";
-$db_name = "contact_manager";
 $username = "Team24";
 $password = "databasekey";
+$db_name = "contact_manager";
 
-    //Read incoming request data
-    $inData = getRequestInfo();
+//Read incoming request data
+$inData = getRequestInfo();
 
-    // Result variables
-    $searchResults = "";
-    $searchCount = 0;
+// Result variables
+$searchResults = "";
+$searchCount = 0;
 
-    $conn = new mysqli($host, $db_name, $username, $password);
+$conn = new mysqli($host, $db_name, $username, $password);
 
     // If connection error (database failure)
-    if ($conn->connect_error)
+if ($conn->connect_error)
+{
+    returnWithError($conn->connect_error);
+}
+else
+{
+    // SQL query statement 
+    $stmt = $conn->prepare(
+        "SELECT id, first_name, last_name, phone, email, address, created_at /* returns these columns for each matching row */
+        FROM contacts /* pull data from contacts table */
+        WHERE user_id = ? /* only returns contacts that belongs to a specific user -> ? will be replaced with current user's ID */
+        AND (first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR phone LIKE ?)" /* Second filter, match search term in at least one of these fields */
+    );
+    // Search through first and last names, allowing partial matches
+    $searchPattern = "%" . $search . "%"
+    // $namePattern = "%" . $inData["search"] . "%";
+
+    $stmt->bind_param("ss", $namePattern, $namePattern);
+    $stmt->execute();
+
+    // Get set of results
+    $result = $stmt->get_result();
+
+    // Loop through results
+    while($row = $stmt->fetch_assoc())
     {
-        returnWithError($conn->connect_error);
+        if ($searchCount > 0)
+        {
+            $searchResults .= ",";
+        }
+
+        $searchCount++;
+        $searchResults.= '"' . $row["first_name"] . $row["last_name" . '"'];
+    }
+
+    if ($searchCount == 0)
+    {
+        returnWithError("No Records Found")
     }
     else
     {
-        // SQL query statement 
-        $stmt = $conn->prepare(
-            "SELECT id, first_name, last_name, phone, email, address, created_at # returns these columns for each matching row 
-            FROM contacts # pull data from contacts table
-            WHERE user_id = ? # only returns contacts that belongs to a specific user -> ? will be replaced with current user's ID
-            AND (first_name LIKE ? OR last_name LIKE ? OR email LIKE ? OR phone LIKE?)" # Second filter, match search term in at least one of these fields
-        );
-        // Search through first and last names, allowing partial matches
-        $namePattern = "%" . $inData["search"] . "%";
-        $stmt->bind_param("ss", $namePattern, $namePattern);
-        $stmt->execute();
-
-        // Get set of results
-        $result = $stmt->get_result();
-
-        // Loop through results
-        while($row = $stmt->fetch_assoc())
-        {
-            if ($searchCount > 0)
-            {
-                $searchResults .= ",";
-            }
-
-            $searchCount++;
-            $searchResults.= '"' . $row["first_name"] . $row["last_name" . '"'];
-        }
-
-        if ($searchCount == 0)
-        {
-            returnWithError("No Records Found")
-        }
-        else
-        {
-            returnWithInfo($searchResults);
-        }
-
-        $stmt->close();
-        $conn->close();
+        returnWithInfo($searchResults);
     }
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'),true);
-    }
+    $stmt->close();
+    $conn->close();
+}
 
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'),true);
+}
 
-    function returnWithError($err)
-    {
-        $retVal = '{"firstName":"","lastName":"","error":"' . $err . '"}';
-        sendResultInfoAsJson($retVal);
-    }
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-    function returnWithInfo($searchResults)
-    {
-        $retVal = '{"results":[' . $searchResults . '],"errpr":""}';
-        sendResultInfoAsJson($retVal);
-    }
+function returnWithError($err)
+{
+    $retVal = '{"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson($retVal);
+}
+
+function returnWithInfo($searchResults)
+{
+    $retVal = '{"results":[' . $searchResults . '],"errpr":""}';
+    sendResultInfoAsJson($retVal);
+}
 ?>
